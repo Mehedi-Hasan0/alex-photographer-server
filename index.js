d4f8f1393eb1624c58480c0a1a5f101241b30f16
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2022-11-10 04:11:13 +0600

    mongodb setup done and added services and limit services api call req

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.69zqaep.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db('alex').collection('services');
+
+        app.get('/limitedServices', async (req, res) => {
+            const query = {};
+            const sorting = { _id: -1 };
+            const cursor = serviceCollection.find(query, sorting).limit(3);
+            const limitedServices = await cursor.toArray();
+            res.send(limitedServices);
+        });
+
+        app.get('/allServices', async (req, res) => {
+            const query = {};
+            const sorting = { _id: -1 };
+            const cursor = serviceCollection.find(query, sorting);
+            const allServices = await cursor.toArray();
+            res.send(allServices)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.log(err));
 
 app.get('/', (req, res) => {
     res.send('Alex-Photography server is running')
